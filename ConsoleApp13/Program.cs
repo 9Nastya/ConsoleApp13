74802191174f3c5d08c8e6de262cbc13a71de64c
Nastya Bilyk <nastyabilyk00@gmail.com>
2023-10-11 19:49:30 +0300

    add line 3

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp13
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
